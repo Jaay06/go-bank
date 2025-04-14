719b32c6bc143d9e13638813fcfcf4dedd1517ef
Joseph <josekoyi9@gmail.com>
2025-04-14 23:25:44 +0100

    chore: move helper

--- api.go
+++ api.go
@@ -9,27 +9,7 @@ import (
 	"github.com/gorilla/mux"
 )
 
-func WriteJSON(w http.ResponseWriter, status int, v any) error{
-	w.WriteHeader(status)
-	w.Header().Set("Content-Type", "application/json")
-	return json.NewEncoder(w).Encode(v)
-}
 
-type apiFunc func(http.ResponseWriter, *http.Request) error
-
-type ApiError struct {
-	Error string
-}
-
-func makeHTTPHandleFunc(f apiFunc) http.HandlerFunc{
-	return func(w http.ResponseWriter, r *http.Request){
-		if err := f(w, r); err != nil{
-			//handle error
-			WriteJSON(w, http.StatusBadRequest, ApiError{Error: err.Error()})
-			
-		}
-	}
-}
 
 type APIServer struct {
 	listenAddr string
@@ -94,3 +74,26 @@ func (s *APIServer) handleTransfer(w http.ResponseWriter, r *http.Request) error
 	return nil
 }
 
+
+/*helper functions */
+func WriteJSON(w http.ResponseWriter, status int, v any) error{
+	w.WriteHeader(status)
+	w.Header().Set("Content-Type", "application/json")
+	return json.NewEncoder(w).Encode(v)
+}
+
+type apiFunc func(http.ResponseWriter, *http.Request) error
+
+type ApiError struct {
+	Error string
+}
+
+func makeHTTPHandleFunc(f apiFunc) http.HandlerFunc{
+	return func(w http.ResponseWriter, r *http.Request){
+		if err := f(w, r); err != nil{
+			//handle error
+			WriteJSON(w, http.StatusBadRequest, ApiError{Error: err.Error()})
+			
+		}
+	}
+}

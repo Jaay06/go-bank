e428fbddfd4bc95b59512cf7be3f87988a4538dc
Joseph <josekoyi9@gmail.com>
2025-04-15 00:47:01 +0100

    feat: database init

--- main.go
+++ main.go
@@ -1,6 +1,17 @@
 package main
 
+import (
+	"log"
+)
+
 func main(){
-server := NewAPIServer(":3000")
-server.Run()
+	store, err := NewPostgresStore()
+	if err != nil {
+		log.Fatal(err)
+	}
+	// test if server is active
+	//fmt.Printf("%+v\n", store)
+
+	server := NewAPIServer(":3000", store)
+	server.Run()
 }
